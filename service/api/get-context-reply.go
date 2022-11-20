8b4152647baa4a2b74dc2353c1e36aeb04d2be8a
CiZ <ffrancy01@gmail.com>
2022-11-20 16:30:19 +0100

    first commit

---
+++ service/api/get-context-reply.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+
+	"git.francescofazzari.it/wasa_photo/service/api/reqcontext"
+	"github.com/julienschmidt/httprouter"
+)
+
+// getContextReply is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
+// handler accepts a reqcontext.RequestContext (see httpRouterHandler).
+func (rt *_router) getContextReply(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	w.Header().Set("content-type", "text/plain")
+	_, _ = w.Write([]byte("Hello World!"))
+}
